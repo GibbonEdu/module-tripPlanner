8d8bc575542c9b5be8adedca0cee8bc0d4c28938
Ray Clark <raynichc@users.noreply.github.com>
2018-05-31 18:01:35 +0800

    v0.2.10

--- manifest.php
+++ manifest.php
@@ -23,7 +23,7 @@ $description = "A trip planner module for Gibbon.";
 $entryURL = "trips_manage.php";
 $type = "Additional";
 $category = "Learn"; 
-$version = "0.2.01"; 
+$version = "0.2.10"; 
 $author = "Ray Clark"; 
 $url = "https://github.com/raynichc/Trip-Planner";
 
