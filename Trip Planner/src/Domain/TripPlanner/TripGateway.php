3b08b49d18b57f8b7250323eb9caecaf5f819dd5
Jim Speir <jimbo80982@gmail.com>
2020-10-17 17:30:20 +0100

    - General clearup - phpcbf PSR2 formatting amendments - Breadcrumbs now fixed - Removing some other SQL

--- Trip Planner/src/Domain/TripPlanner/TripGateway.php
+++ Trip Planner/src/Domain/TripPlanner/TripGateway.php
@@ -1,24 +1,26 @@
 <?php
 
-namespace Gibbon\Domain\TripPlanner;
+namespace Gibbon\Module\TripPlanner\Domain;
+
 use Gibbon\Domain\Traits\TableAware;
 use Gibbon\Domain\QueryCriteria;
 use Gibbon\Domain\QueryableGateway;
 
-class TripGateway extends QueryableGateway {
-  use TableAware;
+class TripGateway extends QueryableGateway
+{
+    use TableAware;
 
-  private static $tableName = 'tripPlannerRequests';
-  private static $primaryKey = 'gibbonTripPlannerID';
-  private static $searchableColumns = [];
+    private static $tableName = 'tripPlannerRequests';
+    private static $primaryKey = 'gibbonTripPlannerID';
+    private static $searchableColumns = [];
 
-  public function queryTrips(QueryCriteria $criteria)
-  {
-    $query = $this
-      ->newQuery()
-      ->from('tripPlannerRequests')
-      ->innerJoin('gibbonPerson','gibbonPerson.gibbonPersonID = tripPlannerRequests.creatorPersonID')
-      ->cols([
+    public function queryTrips(QueryCriteria $criteria)
+    {
+        $query = $this
+        ->newQuery()
+        ->from('tripPlannerRequests')
+        ->innerJoin('gibbonPerson', 'gibbonPerson.gibbonPersonID = tripPlannerRequests.creatorPersonID')
+        ->cols([
         'tripPlannerRequests.tripPlannerRequestID',
         'tripPlannerRequests.creatorPersonID',
         'tripPlannerRequests.timestampCreation',
@@ -34,72 +36,67 @@ class TripGateway extends QueryableGateway {
         'gibbonPerson.preferredName',
         'gibbonPerson.surname',
         '(SELECT startDate FROM tripPlannerRequestDays WHERE tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID ORDER BY startDate ASC LIMIT 1) as firstDayOfTrip'
-      ]);
+        ]);
 
-    $criteria->addFilterRules([
-      'status' => function($query,$status) {
-        if($status != 'All')
-        {
-          return $query
-            ->where('tripPlannerRequests.status = :status')
-            ->bindValue('status',$status);
-        }
-        else return $query;
-      },
-      'eutfilter' => function($query,$eutfilter) use ($criteria) {
-        //expiredUnapprovedFilter, set in the module settings
-        $query
-          ->where('tripPlannerRequestDays.startDate IS NULL')
-          ->where("tripPlannerRequests.status != 'Approved'");
-      },
-
-      'schoolYearID' => function($query,$gibbonYearGroupID) {
-        return $query
-          ->where('tripPlannerRequests.gibbonYearGroupID = :gibbonYearGroupID')
-          ->bindValue('gibbonYearGroupID',$gibbonYearGroupID);
-      },
-      'relation' => function($query,$relation) {
-        $relarr = explode(':',$relation);
-        switch($relation[0])
-        {
-          case 'MR':
-            //My requests option
-            //Only show requests owned by
-            return $query
-              ->where('tripPlannerRequests.creatorPersonID = :personID')
-              ->bindValue('personID',$relation[1]);
-
-          case 'I':
-            //Involved option
-            return $query
-              ->innerJoin('tripPlannerRequestPerson','tripPlannerRequestPerson.tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID')
-              ->where("tripPlannerRequestPerson.role = 'Teacher'")
-              ->where("(tripPlannerRequestPerson.gibbonPersonID = :personID OR triipPlannerRequests.teacherPersonIDs LIKE CONCAT('%',:personID,'%'))")
-              ->bindValues($relation[1]);
-
-          case 'AMA':
-            //Awaiting my approval
-            return $query
-              ->where('EXISTS (SELECT tripPlannerApprovers.gibbonPersonID FROM tripPlannerApprovers WHERE tripPlannerApprovers.gibbonPersonID = :personID)')
-              ->bindValue('personID',$relation[1]);
-
-          default:
-            if(substr($relation[0],0,2) == "DR")
-            {
-              //Department Requests
-              return $query
-                ->innerJoin('gibbonDepartmentStaff','gibbonDepartmentStaff.gibbonPersonID = tripPlannerRequests.creatorPersonID')
-                ->where('gibbonDepartmentStaff.gibbonDepartmentID = :departmentID')
-                ->bindValue('departmentID',substr($relation[0],2));
+        $criteria->addFilterRules([
+        'status' => function ($query, $status) {
+            if ($status != 'All') {
+                return $query
+                ->where('tripPlannerRequests.status = :status')
+                ->bindValue('status', $status);
+            } else {
+                return $query;
             }
-            //Don't filter requests, do nothing
-            return $query;
+        },
+        'eutfilter' => function ($query, $eutfilter) use ($criteria) {
+          //expiredUnapprovedFilter, set in the module settings
+            $query
+            ->where('tripPlannerRequestDays.startDate IS NULL')
+            ->where("tripPlannerRequests.status != 'Approved'");
+        },
+
+        'schoolYearID' => function ($query, $gibbonYearGroupID) {
+            return $query
+            ->where('tripPlannerRequests.gibbonYearGroupID = :gibbonYearGroupID')
+            ->bindValue('gibbonYearGroupID', $gibbonYearGroupID);
+        },
+        'relation' => function ($query, $relation) {
+            $relarr = explode(':', $relation);
+            switch ($relation[0]) {
+                case 'MR':
+                  //My requests option
+                  //Only show requests owned by
+                    return $query
+                ->where('tripPlannerRequests.creatorPersonID = :personID')
+                ->bindValue('personID', $relation[1]);
+
+                case 'I':
+                  //Involved option
+                    return $query
+                ->innerJoin('tripPlannerRequestPerson', 'tripPlannerRequestPerson.tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID')
+                ->where("tripPlannerRequestPerson.role = 'Teacher'")
+                ->where("(tripPlannerRequestPerson.gibbonPersonID = :personID OR triipPlannerRequests.teacherPersonIDs LIKE CONCAT('%',:personID,'%'))")
+                ->bindValues($relation[1]);
+
+                case 'AMA':
+                  //Awaiting my approval
+                    return $query
+                ->where('EXISTS (SELECT tripPlannerApprovers.gibbonPersonID FROM tripPlannerApprovers WHERE tripPlannerApprovers.gibbonPersonID = :personID)')
+                ->bindValue('personID', $relation[1]);
+
+                default:
+                    if (substr($relation[0], 0, 2) == "DR") {
+                      //Department Requests
+                        return $query
+                        ->innerJoin('gibbonDepartmentStaff', 'gibbonDepartmentStaff.gibbonPersonID = tripPlannerRequests.creatorPersonID')
+                        ->where('gibbonDepartmentStaff.gibbonDepartmentID = :departmentID')
+                        ->bindValue('departmentID', substr($relation[0], 2));
+                    }
+                  //Don't filter requests, do nothing
+                    return $query;
+            }
         }
-      }
-    ]);
-    return $this->runQuery($query,$criteria);
-  }
-
+        ]);
+        return $this->runQuery($query, $criteria);
+    }
 }
-
-?>
--- Trip Planner/trips_manage.php
+++ Trip Planner/trips_manage.php
@@ -23,7 +23,10 @@ include "./modules/Trip Planner/src/Domain/TripPlanner/TripGateway.php";
 use Gibbon\Forms\Form;
 use Gibbon\Tables\DataTable;
 use Gibbon\Services\Format;
-use Gibbon\Domain\TripPlanner\TripGateway;
+use Gibbon\Module\TripPlanner\Domain\TripGateway;
+#use Gibbon\Module\TripPlanner\Domain;
+use Gibbon\Domain\School\SchoolYearGateway;
+
 if (!isActionAccessible($guid, $connection2, '/modules/Trip Planner/trips_manage.php')) {
     print "<div class='error'>";
         print "You do not have access to this action.";
@@ -31,9 +34,7 @@ if (!isActionAccessible($guid, $connection2, '/modules/Trip Planner/trips_manage
 } else {
     $highestAction = getHighestGroupedAction($guid, '/modules/Trip Planner/trips_manage.php', $connection2);
     if ($highestAction != false) {
-        print "<div class='trail'>";
-            print "<div class='trailHead'><a href='" . $_SESSION[$guid]["absoluteURL"] . "'>" . _("Home") . "</a> > <a href='" . $_SESSION[$guid]["absoluteURL"] . "/index.php?q=/modules/" . getModuleName($_GET["q"]) . "/" . getModuleEntry($_GET["q"], $connection2, $guid) . "'>" . _(getModuleName($_GET["q"])) . "</a> > </div><div class='trailEnd'>" . _('Manage Trip Requests') . "</div>";
-        print "</div>";
+        $page->breadcrumbs->add(__('Manage Trip Requests'));
 
         if (isset($_GET['return'])) {
             returnProcess($guid, $_GET['return'], null, null);
@@ -46,10 +47,6 @@ if (!isActionAccessible($guid, $connection2, '/modules/Trip Planner/trips_manage
         $ama = (isApprover($connection2, $_SESSION[$guid]["gibbonPersonID"]) && $expenseApprovalType == "Chain Of All") || ($riskAssessmentApproval && isApprover($connection2, $_SESSION[$guid]["gibbonPersonID"], true));
         $departments = getHOD($connection2, $_SESSION[$guid]["gibbonPersonID"]);
         $isHOD = $departments->rowCount() > 0;
-
-        $data = array();
-        $sql = "SELECT tripPlannerRequests.tripPlannerRequestID, tripPlannerRequests.timestampCreation, tripPlannerRequests.title, tripPlannerRequests.description, tripPlannerRequests.status, gibbonPerson.preferredName, gibbonPerson.surname, gibbonPerson.gibbonPersonID FROM tripPlannerRequests LEFT JOIN gibbonPerson ON tripPlannerRequests.creatorPersonID = gibbonPerson.gibbonPersonID";
-        $connector = " WHERE ";
 
         $relations = array();
         $relationFilter = "MR";
@@ -88,55 +85,9 @@ if (!isActionAccessible($guid, $connection2, '/modules/Trip Planner/trips_manage
             $relationFilter = $_POST["relationFilter"];
         }
 
-
-        //This must be the FIRST filter check!
-        if ($relationFilter == "I") {
-            $data["teacherPersonID"] = $_SESSION[$guid]["gibbonPersonID"];
-            $sql .= " JOIN tripPlannerRequestPerson ON (tripPlannerRequestPerson.tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID) WHERE (tripPlannerRequestPerson.role='Teacher' AND :teacherPersonID = tripPlannerRequestPerson.gibbonPersonID OR teacherPersonIDs LIKE CONCAT('%', :teacherPersonID, '%'))";
-            $connector = " AND ";
-        } elseif ($relationFilter == "MR") {
-            $data["creatorPersonID"] = $_SESSION[$guid]["gibbonPersonID"];
-            $sql .= $connector . "tripPlannerRequests.creatorPersonID=:creatorPersonID";
-            if ($connector == " WHERE ") {
-                $connector = " AND ";
-            }
-        } elseif ($relationFilter == "AMA") {
-            $statusFilter = "All";
-        } elseif (strpos($relationFilter, "DR") !== false) {
-            $data["gibbonDepartmentID"] = substr($relationFilter, 2);
-            $sql .= $connector . ":gibbonDepartmentID IN (SELECT gibbonDepartmentID FROM gibbonDepartmentStaff WHERE gibbonPersonID = tripPlannerRequests.creatorPersonID)";
-            if ($connector == " WHERE ") {
-                $connector = " AND ";
-            }
-        }
-
-        if ($statusFilter != "All") {
-            $data["status"] = $statusFilter;
-            $sql .= $connector . "tripPlannerRequests.status=:status";
-            if ($connector == " WHERE ") {
-                $connector = " AND ";
-            }
-        }
-
-        if ($yearFilter != "All Years") {
-            $data["gibbonSchoolYearID"] = $yearFilter;
-            $sql .= $connector . "tripPlannerRequests.gibbonSchoolYearID=:gibbonSchoolYearID";
-            if ($connector == " WHERE ") {
-                $connector = " AND ";
-            }
-        }
-
-        try {
-            $sqlYear = "SELECT gibbonSchoolYearID, name FROM gibbonSchoolYear";
-            $resultYear = $connection2->prepare($sqlYear);
-            $resultYear->execute();
-        } catch (PDOException $e) {
-        }
-
-        $years = array();
-
-        while ($row = $resultYear->fetch()) {
-            $years[$row['gibbonSchoolYearID']] = $row['name'];
+        $schoolYearGateway = $container->get(SchoolYearGateway::class);
+        foreach ($schoolYearGateway->querySchoolYears($schoolYearGateway->newQueryCriteria())->toArray() as $year) {
+            $years[$year['gibbonSchoolYearID']] = $year['name'];
         }
 
         $form = Form::create("tripFilters", $_SESSION[$guid]["absoluteURL"] . "/index.php?q=" . $_GET["q"]);
@@ -160,76 +111,55 @@ if (!isActionAccessible($guid, $connection2, '/modules/Trip Planner/trips_manage
             $row->addSubmit();
 
         print $form->getOutput();
-
-        try {
-            $result = $connection2->prepare($sql);
-            $result->execute($data);
-        } catch (PDOException $e) {
-        }
         
         $tripGateway = $container->get(TripGateway::class);
-        $criteria = $tripGateway->newQueryCriteria()
-              ->filterBy('status',$statusFilter)
-              ->filterBy('relation',$relationFilter.':'.$gibbon->session->get('gibbonPersonID'))
+        $criteria = $tripGateway->newQueryCriteria(true)
+              ->filterBy('status', $statusFilter)
+              ->filterBy('relation', $relationFilter.':'.$gibbon->session->get('gibbonPersonID'))
               ->filterBy('year')
-             ->filterBy('eutfilter',$eutFilter)
+             ->filterBy('eutfilter', $eutFilter)
           ->fromPOST();
 
         $trips = $tripGateway->queryTrips($criteria);
 
-        $table = DataTable::createPaginated('trips',$criteria);
+        $table = DataTable::createPaginated('trips', $criteria);
         $table->setTitle(__("Requests"));
         $table
-          ->addHeaderAction('add',__('Submit Request'))
+          ->addHeaderAction('add', __('Submit Request'))
           ->setURL('/modules/Trip Planner/trips_submitRequest.php');
-        $table->addColumn('tripTitle',__('Title'));
-        $table->addColumn('description',__('Description'));
+        $table->addColumn('tripTitle', __('Title'));
+        $table->addColumn('description', __('Description'));
         $table
-          ->addColumn('owner',__('Owner'))
-          ->format(function($row) {
-            return Format::name($row['title'],$row['preferredName'],$row['surname']);
+          ->addColumn('owner', __('Owner'))
+          ->format(function ($row) {
+            return Format::name($row['title'], $row['preferredName'], $row['surname']);
           });
-        $table->addColumn('status',__('Status'));
+        $table->addColumn('status', __('Status'));
         $table
           ->addActionColumn()
           ->addParam('tripPlannerRequestID')
-          ->format(function($row,$actions) use($connection2,$gibbon) {
+          ->format(function ($row, $actions) use ($connection2, $gibbon) {
               $actions
-                ->addAction('view',__('View Details'))
+                ->addAction('view', __('View Details'))
                 ->setURL('/modules/Trip Planner/trips_requestView.php');
 
-              if(
-                  $gibbon->session->get('gibbonPersonID') == $row['creatorPersonID'] &&
+            if ($gibbon->session->get('gibbonPersonID') == $row['creatorPersonID'] &&
                   $row['status'] != 'Cancelled' &&
                   $row['status'] != 'Rejected'
-              )
-              {
+              ) {
                 $actions
-                  ->addAction('edit',__('Edit'))
-                  ->addParam('mode','edit')
-                  ->setURL('/modules/Trip Planner/trips_submitRequest.php');
-              }
-
-              if(isApprover($connection2,$gibbon->session->get('gibbonPersonID')))
-              {
-                $actions
-                  ->addAction('approve',__('Approve/Reject'))
-                  ->setURL('/modules/Trip Planner/trips_requestApprove.php')
-                  ->setIcon('iconTick');
-              }
-          });
-        /*
-        $table->modifyRows(function($row) use ($eutFilter,$connection2) {
-            if ($eutFilter) {
-                $startDate = getFirstDayOfTrip($connection2, $row["tripPlannerRequestID"]);
-                if (strtotime($startDate) < mktime(0, 0, 0) && $row["status"] != "Approved") {
-                    return $row;
-                }
+                ->addAction('edit', __('Edit'))
+                ->addParam('mode', 'edit')
+                ->setURL('/modules/Trip Planner/trips_submitRequest.php');
             }
-            return $row;
-        });
-         */
-        echo $table->render($trips);
+
+            if (isApprover($connection2, $gibbon->session->get('gibbonPersonID'))) {
+                $actions
+                ->addAction('approve', __('Approve/Reject'))
+                ->setURL('/modules/Trip Planner/trips_requestApprove.php')
+                ->setIcon('iconTick');
+            }
+          });
+          echo $table->render($trips);
     }
 }
-?>
