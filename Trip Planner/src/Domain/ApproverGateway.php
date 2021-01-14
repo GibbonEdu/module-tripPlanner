a0abd5bfcb8ef11ba0e259e384e984017a906847
Ray Clark <raynichc@users.noreply.github.com>
2021-01-14 14:27:29 +1100

    Added selectStaffForApprover method

--- Trip Planner/src/Domain/ApproverGateway.php
+++ Trip Planner/src/Domain/ApproverGateway.php
@@ -4,6 +4,7 @@ namespace Gibbon\Module\TripPlanner\Domain;
 use Gibbon\Domain\Traits\TableAware;
 use Gibbon\Domain\QueryCriteria;
 use Gibbon\Domain\QueryableGateway;
+use Gibbon\Services\Format;
 
 /**
  * Technician Gateway
@@ -29,6 +30,29 @@ class ApproverGateway extends QueryableGateway
             ->leftJoin('gibbonPerson', 'tripPlannerApprovers.gibbonPersonID=gibbonPerson.gibbonPersonID');
 
         return $this->runQuery($query, $critera);
+    }
+
+    public function selectStaffForApprover() {
+        $select = $this
+            ->newSelect()
+            ->from('gibbonPerson')
+            ->cols([
+                'gibbonPerson.gibbonPersonID', 'title', 'surname', 'preferredName', 'username'
+            ])
+            ->innerJoin('gibbonStaff', 'gibbonStaff.gibbonPersonID=gibbonPerson.gibbonPersonID')
+            ->leftJoin($this->getTableName(), 'tripPlannerApprovers.gibbonPersonID=gibbonPerson.gibbonPersonID')
+            ->where("gibbonPerson.status=:status")
+            ->bindValue('status', 'Full')
+            ->where('tripPlannerApprovers.gibbonPersonID IS NULL')
+            ->orderBy(['surname', 'preferredName']);
+
+        $result = $this->runSelect($select);
+        $users = array_reduce($result->fetchAll(), function ($group, $item) {
+            $group[$item['gibbonPersonID']] = Format::name($item['title'], $item['preferredName'], $item['surname'], 'Staff', false, true) . ' (' . $item['username'] . ')';
+            return $group;
+        }, array());
+
+        return $users;
     }
 
     public function updateSequence($order) {
