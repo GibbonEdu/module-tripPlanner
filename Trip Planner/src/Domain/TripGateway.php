26584ac12b62e889d7f75b7236df6aae195c65be
Ray Clark <raynichc@users.noreply.github.com>
2021-01-18 14:52:52 +1100

    Removed legacy columns, added single status filter and extracted relation and eut filters from query criteria

--- Trip Planner/src/Domain/TripGateway.php
+++ Trip Planner/src/Domain/TripGateway.php
@@ -11,11 +11,10 @@ class TripGateway extends QueryableGateway
     use TableAware;
 
     private static $tableName = 'tripPlannerRequests';
-    private static $primaryKey = 'gibbonTripPlannerID';
+    private static $primaryKey = 'tripPlannerRequestID';
     private static $searchableColumns = [];
 
-    public function queryTrips(QueryCriteria $criteria)
-    {
+    public function queryTrips(QueryCriteria $criteria, $gibbonPersonID = null, $relation = null, $eutFilter = false) {
         $query = $this
         ->newQuery()
         ->from('tripPlannerRequests')
@@ -27,87 +26,89 @@ class TripGateway extends QueryableGateway
         'tripPlannerRequests.title as tripTitle',
         'tripPlannerRequests.description',
         'tripPlannerRequests.location',
-        'tripPlannerRequests.date',
-        'tripPlannerRequests.startTime',
-        'tripPlannerRequests.endTime',
         'tripPlannerRequests.status',
-        'tripPlannerRequests.endDate',
         'gibbonPerson.title',
         'gibbonPerson.preferredName',
         'gibbonPerson.surname',
         '(SELECT startDate FROM tripPlannerRequestDays WHERE tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID ORDER BY startDate ASC LIMIT 1) as firstDayOfTrip'
         ]);
 
-        $criteria->addFilterRules([
-          'status' => function ($query, $statuses) {
-            //Whilst, according to https://github.com/auraphp/Aura.SqlQuery/blob/513747a1b399b910f6050e78bd64c3c125a81abf/docs/select.md 
-            //it should be possible to use ->where(<column> IN (:var), ['var' => ['1','2'...]) this caused issues when I tried
-            //Additionally I couldn't pass the array directly in through the criteria filterBy property
-            //This has been reported to Aura in https://github.com/auraphp/Aura.SqlQuery/issues/161#issuecomment-713123581
+        if ($eutFilter) {
+            $query->where('tripPlannerRequestDays.startDate IS NULL')
+                ->where("tripPlannerRequests.status != 'Approved'");
+        }
 
-            $statuses = unserialize($statuses);
-
-            if (!is_array($statuses)) {
-                $statuses = array($statuses);
-            }
-
-            $inClause = '';
-            foreach ($statuses as $key => $status) {
-                $bind = 'status' . $key;
-                $inClause .= ($key > 0 ? ',' : '') . ':' . $bind;
-                $query->bindValue($bind, $status);
-            }
-
-            return $query->where('tripPlannerRequests.status IN (' . $inClause . ')'); 
-        },
-        'eutfilter' => function ($query, $eutfilter) use ($criteria) {
-          //expiredUnapprovedFilter, set in the module settings
-            return $query->where('tripPlannerRequestDays.startDate IS NULL')
-            ->where("tripPlannerRequests.status != 'Approved'");
-        },
-
-        'schoolYearID' => function ($query, $gibbonYearGroupID) {
-            return $query->where('tripPlannerRequests.gibbonYearGroupID = :gibbonYearGroupID')
-            ->bindValue('gibbonYearGroupID', $gibbonYearGroupID);
-        },
-        'relation' => function ($query, $relation) {
-            $relarr = explode(':', $relation);
-            switch ($relation[0]) {
+        if (!empty($relation) && !empty($gibbonPersonID)) {
+            switch ($relation) {
+                //My Requests
                 case 'MR':
-                  //My requests option
-                  //Only show requests owned by
-                    return $query->where('tripPlannerRequests.creatorPersonID = :personID')
-                ->bindValue('personID', $relation[1]);
+                    $query->where('tripPlannerRequests.creatorPersonID = :personID')
+                        ->bindValue('personID', $gibbonPersonID);
+                    break;
 
+                //Involved option
                 case 'I':
-                  //Involved option
-                    return $query->innerJoin('tripPlannerRequestPerson', 'tripPlannerRequestPerson.tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID')
-                ->where("tripPlannerRequestPerson.role = 'Teacher'")
-                ->where("(tripPlannerRequestPerson.gibbonPersonID = :personID OR tripPlannerRequests.teacherPersonIDs LIKE CONCAT('%',:personID,'%'))")
-                ->bindValues($relation[1]);
+                    $query->innerJoin('tripPlannerRequestPerson', 'tripPlannerRequestPerson.tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID')
+                        ->where('tripPlannerRequestPerson.role = :role')
+                        ->bindValue('role', 'Teacher')
+                        ->where('tripPlannerRequestPerson.gibbonPersonID = :personID')
+                        ->bindValue('personID', $gibbonPersonID);
+                    break;
 
+                //Awaiting my approval
                 case 'AMA':
-                  //Awaiting my approval
-                    return $query->where('EXISTS (SELECT tripPlannerApprovers.gibbonPersonID FROM tripPlannerApprovers WHERE tripPlannerApprovers.gibbonPersonID = :personID)')
-                ->bindValue('personID', $relation[1]);
+                    //TODO: Changed to match previous behavior. Until then reverted to checks in php
+                    //$query->where('EXISTS (SELECT tripPlannerApprovers.gibbonPersonID FROM tripPlannerApprovers WHERE tripPlannerApprovers.gibbonPersonID = :personID)')
+                    //    ->bindValue('personID', $gibbonPersonID);
+                    break;
 
                 default:
-                    if (substr($relation[0], 0, 2) == "DR") {
-                      //Department Requests
-                        return $query->innerJoin('gibbonDepartmentStaff', 'gibbonDepartmentStaff.gibbonPersonID = tripPlannerRequests.creatorPersonID')
-                        ->where('gibbonDepartmentStaff.gibbonDepartmentID = :departmentID')
-                        ->bindValue('departmentID', substr($relation[0], 2));
+                    //Department Requests
+                    if (substr($relation, 0, 2) == "DR") {
+                        $query->innerJoin('gibbonDepartmentStaff', 'gibbonDepartmentStaff.gibbonPersonID = tripPlannerRequests.creatorPersonID')
+                            ->where('gibbonDepartmentStaff.gibbonDepartmentID = :departmentID')
+                            ->bindValue('departmentID', substr($relation, 2));
                     }
-                  //Don't filter requests, do nothing
-                    return $query;
+                    break;
             }
-        },
-        'tripDay' => function($query,$queryDate) {
-          return $query->innerJoin('tripPlannerRequestDays','tripPlannerRequestDays.tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID')
-            ->where('tripPlannerRequestDays.startDate <= :queryDate')
-            ->where('tripPlannerRequestDays.endDate >= :queryDate')
-            ->bindValue('queryDate',$queryDate);
         }
+
+        $criteria->addFilterRules([
+            'status' => function($query, $status) {
+                return $query->where('tripPlannerRequests.status = :status')
+                    ->bindValue('status', $status);
+            },
+            'statuses' => function ($query, $statuses) {
+                //Whilst, according to https://github.com/auraphp/Aura.SqlQuery/blob/513747a1b399b910f6050e78bd64c3c125a81abf/docs/select.md 
+                //it should be possible to use ->where(<column> IN (:var), ['var' => ['1','2'...]) this caused issues when I tried
+                //Additionally I couldn't pass the array directly in through the criteria filterBy property
+                //This has been reported to Aura in https://github.com/auraphp/Aura.SqlQuery/issues/161#issuecomment-713123581
+
+                $statuses = unserialize($statuses);
+
+                if (!is_array($statuses)) {
+                    $statuses = array($statuses);
+                }
+
+                $inClause = '';
+                foreach ($statuses as $key => $status) {
+                    $bind = 'status' . $key;
+                    $inClause .= ($key > 0 ? ',' : '') . ':' . $bind;
+                    $query->bindValue($bind, $status);
+                }
+
+                return $query->where('tripPlannerRequests.status IN (' . $inClause . ')'); 
+            },
+            'year' => function ($query, $gibbonSchoolYearID) {
+                return $query->where('tripPlannerRequests.gibbonSchoolYearID = :gibbonSchoolYearID')
+                ->bindValue('gibbonSchoolYearID', $gibbonSchoolYearID);
+            },
+            'tripDay' => function($query, $queryDate) {
+                return $query->innerJoin('tripPlannerRequestDays','tripPlannerRequestDays.tripPlannerRequestID = tripPlannerRequests.tripPlannerRequestID')
+                    ->where('tripPlannerRequestDays.startDate <= :queryDate')
+                    ->where('tripPlannerRequestDays.endDate >= :queryDate')
+                    ->bindValue('queryDate',$queryDate);
+            },
         ]);
         return $this->runQuery($query, $criteria);
     }
