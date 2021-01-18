7cccfc4587699f5c16bc11749c72ab809264e2f5
Ray Clark <raynichc@users.noreply.github.com>
2021-01-18 14:49:56 +1100

    Basic OOification

--- Trip Planner/trips_requestApprove.php
+++ Trip Planner/trips_requestApprove.php
@@ -17,50 +17,51 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-//Module includes
-include "./modules/Trip Planner/moduleFunctions.php";
+use Gibbon\Module\TripPlanner\Domain\TripGateway;
+
+require_once __DIR__ . '/moduleFunctions.php';
+
+$page->breadcrumbs
+        ->add(__('Manage Trip Requests'), 'trips_manage.php')
+        ->add(__('Approve Request'));
 
 if (!isActionAccessible($guid, $connection2, '/modules/Trip Planner/trips_manage.php')) {
     //Acess denied
-    print "<div class='error'>";
-        print "You do not have access to this action.";
-    print "</div>";
+    $page->addError(__('You do not have access to this action.'));
 } else {
-    print "<div class='trail'>";
-        print "<div class='trailHead'><a href='" . $_SESSION[$guid]["absoluteURL"] . "'>" . _("Home") . "</a> > <a href='" . $_SESSION[$guid]["absoluteURL"] . "/index.php?q=/modules/" . getModuleName($_GET["q"]) . "/" . getModuleEntry($_GET["q"], $connection2, $guid) . "'>" . _(getModuleName($_GET["q"])) . "</a> > <a href='" . $_SESSION[$guid]["absoluteURL"] . "/index.php?q=/modules/Trip Planner/trips_manage.php'>" . _("Manage Trip Requests") . "</a> > </div><div class='trailEnd'>" . _('Approve Request') . "</div>";
-    print "</div>";
-
     if (isset($_GET['return'])) {
         returnProcess($guid, $_GET['return'], null, null);
     }
 
-    if (isset($_GET["tripPlannerRequestID"])) {
-        $tripPlannerRequestID = $_GET["tripPlannerRequestID"];
+    $tripGateway = $container->get(TripGateway::class);
+
+    $tripPlannerRequestID = $_GET['tripPlannerRequestID'] ?? '';
+    $trip = $tripGateway->getByID($tripPlannerRequestID);
+
+    if (empty($trip) {
+        $page->addError(__('Invalid Trip Request Selected.'));
+    } else {
         if (($approvalReturn = needsApproval($connection2, $tripPlannerRequestID, $_SESSION[$guid]["gibbonPersonID"])) == 0 || ($status == "Awaiting Final Approval" && isApprover($connection2, $_SESSION[$guid]["gibbonPersonID"], true))) {
             renderTrip($guid, $connection2, $tripPlannerRequestID, true);
         } else {
-            if($approvalReturn == 2 || $approvalReturn == 1) {
-                print "<div class='error'>";
-                    if($approvalReturn == 1) {
-                        print "A Database error occured.";
-                    } else {
-                        print "You do not have access to this action.";
-                    }
-                print "</div>";
-            } else if($approvalReturn == 3 || $approvalReturn == 5) {
-                print "<div class='warning'>";
-                    if($approvalReturn == 3) {
-                        print "The trip has already been approved.";
-                    } else {
-                        print "You have already approved this trip.";
-                    }
-                print "</div>";
+            switch ($approvalReturn) {
+                case 1:
+                    $page->addError(__('A Database error occured.'));
+                    break;
+
+                case 2:
+                    $page->addError(__('You do not have access to this action.'));
+                    break;
+
+                case 3:
+                    $page->addWarning(__('The trip has already been approved.'));
+                    break;
+
+                case 4:
+                    $page->addWarning(__('You have already approved this trip.'));
+                    break;
             }
         }
-    } else {    
-        print "<div class='error'>";
-            print "No request selected.";
-        print "</div>";
-    }
+    } 
 }   
 ?>
