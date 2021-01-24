d23b1d6a3e772d812b4e910c65a0c30ddc38baca
Ray Clark <raynichc@users.noreply.github.com>
2021-01-24 23:15:38 +1100

    Migrated to gateway

--- Trip Planner/trips_requestViewProcess.php
+++ Trip Planner/trips_requestViewProcess.php
@@ -1,54 +1,77 @@
 <?php
 
-require_once '../../gibbon.php';
-require_once "./moduleFunctions.php";
+use Gibbon\Module\TripPlanner\Domain\TripGateway;
+use Gibbon\Module\TripPlanner\Domain\TripLogGateway;
 
-$URL = $_SESSION[$guid]["absoluteURL"] . "/index.php?q=/modules/Trip Planner/";
+require_once '../../gibbon.php';
+require_once './moduleFunctions.php';
+
+$moduleName = $gibbon->session->get('module');
+
+$URL = $gibbon->session->get('absoluteURL') . '/index.php?q=/modules/' . $moduleName;
 
 if (!isActionAccessible($guid, $connection2, '/modules/Trip Planner/trips_manage.php')) {
     //Acess denied
-    $URL .= "trips_manage.php&return=error0";
+    $URL .= '/trips_manage.php&return=error0';
     header("Location: {$URL}");
     exit();
 } else {
-    if (isset($_POST["tripPlannerRequestID"])) {
-        $tripPlannerRequestID = $_POST["tripPlannerRequestID"];
-    } else {
-        $URL .= "trips_manage.php&return=error1";
+    $tripPlannerRequestID = $_POST['tripPlannerRequestID'] ?? '';
+
+    $tripGateway = $container->get(TripGateway::class);
+
+    if (empty($tripPlannerRequestID) || !$tripGateway->exists($tripPlannerRequestID)) {
+        $URL .= '/trips_manage.php&return=error1';
         header("Location: {$URL}");
         exit();
     }
 
-    $gibbonPersonID = $_SESSION[$guid]["gibbonPersonID"];
-
+    $gibbonPersonID = $gibbon->session->get('gibbonPersonID');
     $highestAction = getHighestGroupedAction($guid, '/modules/Trip Planner/trips_manage.php', $connection2);
+
     if (hasAccess($container, $tripPlannerRequestID, $gibbonPersonID, $highestAction)) {
-        $URL .= "trips_requestView.php&tripPlannerRequestID=" . $tripPlannerRequestID;
-        if (isset($_POST["comment"])) {
-            $comment = $_POST["comment"];
-            if ($comment == "" || $comment == null) {
-                $URL .= "&return=error1";
-                header("Location: {$URL}");
-                exit();
-            }
-        } else {
-            $URL .= "&return=error1";
+        $URL .= '/trips_requestView.php&tripPlannerRequestID=' . $tripPlannerRequestID;
+
+        $comment = $_POST['comment'] ?? '';
+
+        if (empty($comment)) {
+            $URL .= '&return=error1';
             header("Location: {$URL}");
             exit();
         }
 
-        if (!logEvent($connection2, $tripPlannerRequestID, $gibbonPersonID, "Comment", $comment)) {
-            $URL .= "&return=error2";
+        $logGateway = $container->get(TripLogGateway::class);
+
+        $tripPlannerRequestLogID = $logGateway->insert([
+            'tripPlannerRequestID'  => $tripPlannerRequestID,
+            'gibbonPersonID'        => $gibbonPersonID,
+            'action'                => 'Comment',
+            'comment'               => $comment
+        ]);
+
+        if (!$tripPlannerRequestID) {
+            $URL .= '&return=error2';
             header("Location: {$URL}");
             exit();
         }
-        requestNotification($guid, $connection2, $tripPlannerRequestID, $_SESSION[$guid]["gibbonPersonID"], "Comment");
 
-        $URL .= "&return=success0";
+        $notificationGateway = $container->get(NotificationGateway::class);
+        $notificationSender = new NotificationSender($notificationGateway, $gibbon->session);
+
+        $text = __('Someone has commented on a trip request.');
+
+        $people = $logGateway->selectLoggedPeople($tripPlannerRequestID);
+        while ($row = $people->fetch()) {
+            $notificationSender->addNotification($row['gibbonPersonID'], $text, $moduleName, $URL);
+        }
+
+        $notificationSender->sendNotifications();
+
+        $URL .= '&return=success0';
         header("Location: {$URL}");
         exit();
     } else {
-        $URL .= "trips_manage.php&return=error0";
+        $URL .= '/trips_manage.php&return=error0';
         header("Location: {$URL}");
         exit();
     }
