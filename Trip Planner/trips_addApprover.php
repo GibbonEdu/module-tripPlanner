965a7761341ed7147a1d802973d8c6d3bf60bd23
Ross Parker <>
2021-11-02 13:43:22 +0800

    v1.4.09

--- Trip Planner/trips_addApprover.php
+++ Trip Planner/trips_addApprover.php
@@ -19,6 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use Gibbon\Forms\Form;
 use Gibbon\Forms\DatabaseFormFactory;
+use Gibbon\Domain\System\SettingGateway;
 use Gibbon\Module\TripPlanner\Domain\ApproverGateway;
 
 $page->breadcrumbs
@@ -45,7 +46,7 @@ if (!isActionAccessible($guid, $connection2, '/modules/Trip Planner/trips_addApp
             ->setRequired(true)
             ->placeholder('Please select...');
 
-    $riskAssessmentApproval = getSettingByScope($connection2, 'Trip Planner', 'riskAssessmentApproval');
+    $riskAssessmentApproval = $container->get(SettingGateway::class)->getSettingByScope('Trip Planner', 'riskAssessmentApproval');
     if($riskAssessmentApproval) {
         $row = $form->addRow();
             $row->addLabel('finalApprover', 'Final Approver');
