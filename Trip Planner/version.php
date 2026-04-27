db5d669b275403a15fa0087a71b4f1205dd5eab3
Ali Alam <aalam@ichk.edu.hk>
2026-04-27 10:59:08 +0800

    Fixed the bug to display the correct list of students in Trip Overview

--- Trip Planner/version.php
+++ Trip Planner/version.php
@@ -22,5 +22,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information
  */
-$moduleVersion = "1.8.07";
+$moduleVersion = "1.8.08";
 $coreVersion = '24.0.00';
