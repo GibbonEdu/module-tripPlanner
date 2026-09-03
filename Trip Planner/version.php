9736e96a437186be1c7b3001a4e5b37a020cf8ad
Ali Alam <aalam@ichk.edu.hk>
2026-09-03 09:22:49 +0800

    v1.8.10 (#44)

--- Trip Planner/version.php
+++ Trip Planner/version.php
@@ -22,5 +22,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information
  */
-$moduleVersion = "1.8.09";
+$moduleVersion = "1.8.10";
 $coreVersion = '24.0.00';
